3c4337a1332bf3af783dea8b0e4a7642d34a12b3
Mina Dodunekova <m.dodunekova@up2.nl>
2018-08-03 17:18:34 +0300

    adding passport

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require( 'express' );
+const passport = require( 'passport' );
+const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
+const keys = require( './config/keys' );
 
 // generate new application
 const app = express();
 
+// client id
+// client secret
+passport.use( new GoogoleStrategy( {
+			clientID: keys.googleCLientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		accessToken => {	
+			console.log( 'accessToken', accessToken );
+		} 
+	) 
+);
 
-// creating route handler handling the request method get
-app.get( '/', ( req, res ) => {
-	res.send( { hi: 'there sweetie' } )
-} );
+app.get( 
+	'/auth/google', 
+	passport.authenticate( 'google', {
+		scope: [ 'profile', 'email' ]
+	} )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen( PORT );
